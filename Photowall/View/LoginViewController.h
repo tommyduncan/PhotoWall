a656102234cc024c5831a4db8dff81934fb128a6
student <student@studentde-iMac-235.local>
2017-05-12 17:19:11 +0800

    Interface builder

---
+++ Photowall/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Photowall
+//
+//  Created by student on 2017/5/12.
+//  Copyright © 2017年 Tommy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
